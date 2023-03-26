688b540d85de2eb820765dfcf23f31ad3c91a8a6
p3trik31 <patrik.ch3@seznam.cz>
2023-03-26 15:58:09 +0200

    commit

---
+++ matika/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace matika
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.VysledekOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(578, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(560, 184);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "výsledek";
+            // 
+            // VysledekOutput
+            // 
+            this.VysledekOutput.Location = new System.Drawing.Point(16, 263);
+            this.VysledekOutput.Name = "VysledekOutput";
+            this.VysledekOutput.Size = new System.Drawing.Size(718, 175);
+            this.VysledekOutput.TabIndex = 4;
+            this.VysledekOutput.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.VysledekOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "idk";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox VysledekOutput;
+    }
+}
+
---
+++ matika/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace matika
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CountWordLengths()
+        {
+            string[] words = richTextBox1.Text.Split(' ');
+
+            Dictionary<int, int> wordLengths = new Dictionary<int, int>();
+
+            foreach (string word in words)
+            {
+                int length = 0;
+                foreach (char c in word)
+                {
+                    if (char.IsLetter(c))
+                    {
+                        length++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (length > 0)
+                {
+                    if (wordLengths.ContainsKey(length))
+                    {
+                        wordLengths[length]++;
+                    }
+                    else
+                    {
+                        wordLengths.Add(length, 1);
+                    }
+                }
+            }
+
+            
+            foreach (KeyValuePair<int, int> entry in wordLengths)
+            {
+                
+                VysledekOutput.AppendText(string.Format("{0} písmen ve slově: {1}{2}", entry.Key, entry.Value, Environment.NewLine));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VysledekOutput.Text = "";
+            CountWordLengths();
+        }
+    }
+
+}
